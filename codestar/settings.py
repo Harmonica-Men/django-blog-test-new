88bc4b3cd1d482a750157678caf5b1b779a22cc4
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-06-20 17:47:35 +0000

    ready to deploy 2

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-harmonicame-djangoblogt-gf6sjmgbq39.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-harmonicame-djangoblogt-gf6sjmgbq39.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
